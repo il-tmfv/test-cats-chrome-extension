27f6241da92a405c84d3649cb24b3acab1d912a8
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-06 09:34:26 +0300

    Added basic words replacement

--- content.js
+++ content.js
@@ -13,8 +13,32 @@ var replaceImagesWithCats = function () {
   }
 };
 
+var putSpanTagAround = function (str) {
+  return '<span class=\'replaced-with-cat\'>' + str + '</span>';
+}
+
+var replaceWord = function (words, newWord) {
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    var re = new RegExp(word, 'g');
+    var taggedNewWord = putSpanTagAround(newWord);
+    document.body.innerHTML = document.body.innerHTML.replace(re, taggedNewWord);
+  }
+}
+
+var replaceWordsWithCats = function () {
+  replaceWord(['новости',   'комментарии'],   'котики');
+  replaceWord(['новостей',  'комментариев'],  'котиков');
+  replaceWord(['новостям',  'комметариям'],   'котикам');
+  replaceWord(['новостями', 'комментариями'], 'котиками');
+  replaceWord(['новостях',  'комментариях'],  'котиках');
+}
+
 chrome.runtime.sendMessage({ type: 'catsStatus' }, function (response) {
   console.log('Got response ' + JSON.stringify(response));
-  if (response.catsStatus == 1) replaceImagesWithCats();
+  if (response.catsStatus == 1) {
+    replaceWordsWithCats();
+    replaceImagesWithCats();
+  }
   return;
 });
