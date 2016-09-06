96d6b7e538f3fe1bda6e68392c09b9650a89975a
Ilya Timofeev <il.tmfv@gmail.com>
2016-09-06 23:45:18 +0300

    Added watching for new img tags on page

--- content.js
+++ content.js
@@ -1,23 +1,28 @@
-var replaceImagesWithCats = function () {
-  var images = document.getElementsByTagName('img');
-  for (var i = 0; i < images.length; i++) {
-    var width = images[i].width;
-    var height = images[i].height;
-
+function replaceImageWithCat(image) {
+  var width = image.width;
+  var height = image.height;
+  if (image.className.indexOf('replaced-with-cat') === -1) {
     if (width > 100 || height > 100) {
       if (width / height < 2 && height / width < 2) {
-        images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+        image.src = 'http://placekitten.com/' + width + '/' + height;
+        image.className += ' replaced-with-cat';
       }
     }
-
   }
-};
+}
 
-var putSpanTagAround = function (str) {
+function replaceImagesWithCats() {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    replaceImageWithCat(images[i]);
+  }
+}
+
+function putSpanTagAround(str) {
   return '<span class=\'replaced-with-cat\'>' + str + '</span>';
 }
 
-var replaceWord = function (words, newWord) {
+function replaceWord(words, newWord) {
   for (var i = 0; i < words.length; i++) {
     var word = words[i];
     var re = new RegExp(word, 'g');
@@ -26,19 +31,42 @@ var replaceWord = function (words, newWord) {
   }
 }
 
-var replaceWordsWithCats = function () {
-  replaceWord(['новости',   'комментарии'],   'котики');
-  replaceWord(['новостей',  'комментариев'],  'котиков');
-  replaceWord(['новостям',  'комметариям'],   'котикам');
+function replaceWordsWithCats() {
+  replaceWord(['новости', 'комментарии'], 'котики');
+  replaceWord(['новостей', 'комментариев'], 'котиков');
+  replaceWord(['новостям', 'комметариям'], 'котикам');
   replaceWord(['новостями', 'комментариями'], 'котиками');
-  replaceWord(['новостях',  'комментариях'],  'котиках');
+  replaceWord(['новостях', 'комментариях'], 'котиках');
 }
 
-chrome.runtime.sendMessage({ type: 'catsStatus' }, function (response) {
-  console.log('Got response ' + JSON.stringify(response));
-  if (response.catsStatus == 1) {
-    replaceWordsWithCats();
-    replaceImagesWithCats();
-  }
-  return;
+var observer = new MutationObserver(function (mutations, observer) {
+  mutations.forEach(function (mutation) {
+    if (mutation.target.tagName.toUpperCase() === 'IMG') {
+      console.dir(mutation);
+      doIfExtensionIsOn(function () {
+        replaceImageWithCat(mutation.target);
+        //replaceWordsWithCats();
+      });
+    }
+  });
+});
+
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
+
+function doIfExtensionIsOn(callback) {
+  chrome.runtime.sendMessage({ type: 'catsStatus' }, function (response) {
+    console.log('Got response ' + JSON.stringify(response));
+    if (response.catsStatus == 1) {
+      callback();
+    }
+    return;
+  });
+}
+
+doIfExtensionIsOn(function () {
+  replaceWordsWithCats();
+  replaceImagesWithCats();
 });
